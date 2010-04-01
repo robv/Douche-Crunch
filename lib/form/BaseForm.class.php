3c341ee2726053f518cde6309058348f79d3d89a
Graham Christensen <graham.christensen@iamgraham.net>
2010-04-01 13:49:24 -0500

    Adding some stuff.

---
+++ lib/form/BaseForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base project form.
+ * 
+ * @package    DoucheCrunch
+ * @subpackage form
+ * @author     Graham Christensen <graham@grahamc.com> 
+ * @version    SVN: $Id: BaseForm.class.php 20147 2009-07-13 11:46:57Z FabianLange $
+ */
+class BaseForm extends sfFormSymfony
+{
+}
