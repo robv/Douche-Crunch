c498bb37ca574b7c2b670f43e1cedbeb56024223
robv <thisisrobv@gmail.com>
2010-04-01 21:13:36 -0500

    Basic styling for homepage.

--- apps/frontend/modules/douche/templates/showSuccess.php
+++ apps/frontend/modules/douche/templates/showSuccess.php
@@ -1,3 +1,60 @@
-<h1><?php echo $douche->getTwitterName(); ?></h1>
-<img src="<?php echo $douche->getImageUrl(); ?>" alt="<?php echo $douche->getTwitterScreenName(); ?>" />
-<?php echo link_to('Yeah, they are!', 'douche_confirm', $douche); ?> | <?php echo link_to('No way!!11!1', 'douche_deny', $douche); ?>
+<div id="title_bar">
+	<h2>Social Media Douchebag or Not?</h2>
+</div>
+
+
+<div id="two_column_content">
+	
+	<div id="two_column_content_left">
+		
+		<div class="next_prev">
+			<div class="prev"><a href="#">Previous</a></div>
+			<div class="next"><a href="#">Next</a></div>
+		</div>
+		
+		<h2 class="screen_name"><a href="http://twiiter.com/">@<?php echo $douche->getTwitterScreenName(); ?></a></h2>
+		
+		<div class="image_wrapper_wrapper">
+			<div class="image_wrapper">
+				<div class="image"><img src="<?php echo $douche->getImageUrl(); ?>" alt="<?php echo $douche->getTwitterName(); ?>" /></div>
+			</div>
+		</div>
+		
+		<div class="vote_buttons">
+			<span class="vote_buttons_yes">
+				<?php echo link_to('Yeah, they are!', 'douche_confirm', $douche); ?>
+			</span>
+			<span class="vote_button_no">
+				<?php echo link_to('No way!!11!1', 'douche_deny', $douche); ?>
+			</span>
+		</div>
+		
+		<div class="hr"><hr /></div>
+		
+		<h2>Their Latest Tweet</h2>
+		
+		<p><?php echo $douche->getLatestTweet(); ?></p>
+		
+	</div>
+	
+	<div id="two_column_content_right">
+		
+		<h2>Are they a Douche?</h2>
+		
+		<form id="douche_check_form">
+			<input type="text" class="douche_check_form_input" />
+			<input type="submit" value="Check Now" class="douche_check_form_submit" />
+		</form>
+		
+		<h2>Douche Leaderboard</h2>
+	
+		<ol>
+			<li><a href="#">Person</a></li>
+			<li>Person</li>
+			<li>Person</li>
+			<li>Person</li>
+		</ol>
+	
+	</div>
+	
+</div>
--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -1,14 +1,32 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-  <head>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
     <?php include_http_metas() ?>
     <?php include_metas() ?>
     <?php include_title() ?>
-    <link rel="shortcut icon" href="/favicon.ico" />
     <?php include_stylesheets() ?>
     <?php include_javascripts() ?>
-  </head>
-  <body>
-    <?php echo $sf_content ?>
-  </body>
+<script type="text/javascript" charset="utf-8">Site.config.site_url = '/'; Site.config.base_url = '/';</script>
+</head>
+
+<body>
+	
+	<div id="header_nav" class="clearfix">
+		<div id="header_nav_inner" class="clearfix">
+			<ul class="clearfix">
+				<li><a href="#">About DoucheCrunch</a></li>
+			</ul>
+		</div>
+	</div>
+	
+	<div id="container_container">
+		
+		<h1>DoucheCrunch</h1>
+    
+	<?php echo $sf_content ?>
+
+	</div>
+
+</body>
+
 </html>
